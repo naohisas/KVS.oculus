cc9106d24b12cff21ff54a9cdc273fac5dd5a799
Naohisa Sakamoto <naohisa.sakamoto@gmail.com>
2019-01-24 14:44:47 +0900

    Update main.cpp

--- App/Test/main.cpp
+++ App/Test/main.cpp
@@ -33,27 +33,4 @@ int main( int argc, char** argv )
     screen.show();
 
     return app.run();
-/*
-    std::cout << "Description: " << kvs::oculus::Description() << std::endl;
-    std::cout << "Version: " << kvs::oculus::Version() << std::endl;
-
-    kvs::oculus::Application app( argc, argv );
-    app.run();
-
-    kvs::oculus::HeadMountedDisplay hmd;
-    hmd.createDebug( ovrHmd_DK2 );
-
-    std::cout << "Product Name: " << hmd.productName() << std::endl;
-    std::cout << "Manufacturer: " << hmd.manufacturer() << std::endl;
-    std::cout << "Vendor ID: " << hmd.vendorId() << std::endl;
-    std::cout << "Product ID: " << hmd.productId() << std::endl;
-    std::cout << "Resolution: " << kvs::oculus::ToVec2i( hmd.resolution() ) << std::endl;
-    std::cout << "Window Position: " << kvs::oculus::ToVec2i( hmd.windowPosition() ) << std::endl;
-
-    hmd.destroy();
-
-    app.quit();
-
-    return 0;
-*/
 }
