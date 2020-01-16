6c94157f54ff416e0c6bb8f3a865934eb72bece2
GingaTabata <1685040t@stu.kobe-u.ac.jp>
2020-01-16 14:05:45 +0900

    Update main.cpp

--- App/ParallelCoordinates3D/main.cpp
+++ App/ParallelCoordinates3D/main.cpp
@@ -121,6 +121,7 @@ int main( int argc, char** argv )
     renderer->setReducedData( reduced_data );
     renderer->setBundledPosition( 0 );
     renderer->setReducedPlaneScale( 3.0f ); //scaling
+    renderer->setCurveSize( 0.8f ); //0 <= CurveSize <= 1
 
 //    kvs::AdaptiveKMeans kmeans;
 //    kmeans.setInputTableData( reduced_data );
