22bd9648189ce4eb16e26af1bfdf60ba9739620f
Ginga Tabata <1685040t@stu.kobe-u.ac.jp>
2020-01-30 18:04:09 +0900

    Update MyTouchController.h

--- App/ParallelCoordinates3D/MyTouchController.h
+++ App/ParallelCoordinates3D/MyTouchController.h
@@ -34,83 +34,9 @@ namespace local
     class MyTouchController : public kvs::EventListener
     {
     public:
-        MyTouchController( kvs::glut::Screen* screen )
-        {
-            setEventType( kvs::EventBase::KeyPressEvent | kvs::EventBase::MousePressEvent );
-            screen->addEvent( this );
-        }
+        MyTouchController( kvs::glut::Screen* screen );
         
-        void keyPressEvent( kvs::KeyEvent* event )
-        {
-            typedef local::BundledParallelCoordinates3DRenderer Renderer;
-            Renderer* renderer = Renderer::DownCast( scene()->renderer("Renderer") );
-            float size = renderer->m_curve_size;
-            float scale = renderer->m_reduced_plane_scale;
-            kvs::TableObject* object = kvs::TableObject::DownCast( scene()->object("Object") );
-            size_t numberOfPlanes = object->numberOfColumns() / 2 - 2;
-            size_t position = renderer->m_bundled_position;
-            switch ( event->key() )
-            {
-                case kvs::Key::z:
-                    std::cout << "BundledPosition mode" << std::endl;
-                    position = position - 1;
-                    if (position > numberOfPlanes )
-                    {
-                        position = 0;
-                    }
-                    std::cout << "Bundled_Position = " << position << std::endl;
-                    renderer->setBundledPosition( position );
-                    break;
-                case kvs::Key::c:
-                    std::cout << "BundledPosition mode" << std::endl;
-                    position = position + 1;
-                    if (position > numberOfPlanes )
-                    {
-                        position = numberOfPlanes;
-                    }
-                    std::cout << "Bundled_Position = " << position << std::endl;
-                    renderer->setBundledPosition( position );
-                    break;
-                case kvs::Key::Down:
-                    std::cout << "CurveSizeScale mode" << std::endl;
-                    size = size - 0.1f;
-                    if (size < 0 )
-                    {
-                        size = 0.0f;
-                    }
-                    std::cout << "Curve_Size = " << size << std::endl;
-                    renderer->setCurveSize( size );
-                    break;
-                case kvs::Key::Up:
-                    std::cout << "CurveSizeScale mode" << std::endl;
-                    size = size + 0.1f;
-                    if (size > 1.0)
-                    {
-                        size = 1.0f;
-                    }
-                    std::cout << "Curve_Size = " << size << std::endl;
-                    renderer->setCurveSize( size );
-                    break;
-                case kvs::Key::s:
-                    std::cout << "ReducedPlaneScale mode" << std::endl;
-                    scale = scale + 0.1f;
-                    std::cout << "Reduced_Plane_Size = " << scale << std::endl;
-                    renderer->setReducedPlaneScale( scale );
-                    break;
-                case kvs::Key::x:
-                    std::cout << "ReducedPlaneScale mode" << std::endl;
-                    scale = scale - 0.1f;
-                    if (scale < 0 )
-                    {
-                        scale = 0.0f;
-                    }
-                    std::cout << "Reduced_Plane_Size = " << scale << std::endl;
-                    renderer->setReducedPlaneScale( scale );
-                    break;
-                default:
-                    break;
-            }
-        }
+        void keyPressEvent( kvs::KeyEvent* event );
         
         void mousePressEvent( kvs::MouseEvent* event )
         {
