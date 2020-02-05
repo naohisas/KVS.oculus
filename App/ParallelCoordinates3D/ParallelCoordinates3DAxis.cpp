64aee5eaf9e7f5cbff85fd45724fc59c1a94278e
Ginga Tabata <1685040t@stu.kobe-u.ac.jp>
2020-02-05 15:51:53 +0900

    Update ParallelCoordinates3DAxis.cpp

--- App/ParallelCoordinates3D/ParallelCoordinates3DAxis.cpp
+++ App/ParallelCoordinates3D/ParallelCoordinates3DAxis.cpp
@@ -2,6 +2,7 @@
 #include <kvs/OpenGL>
 #include <kvs/TableObject>
 #include <algorithm>
+#include <kvs/glut/GLUT>
 
 
 namespace local
@@ -10,7 +11,15 @@ namespace local
 ParallelCoordinates3DAxis::ParallelCoordinates3DAxis():
     m_axis_width( 1.0f ),
     m_axis_color( kvs::RGBColor::Black() ),
-    m_background_color( kvs::RGBColor::White(), 0.0f )
+    m_label_color( kvs::RGBColor::Black() ),
+    m_value_color( kvs::RGBColor::Black() ),
+    m_background_color( kvs::RGBColor::White(), 0.0f ),
+    m_number_of_grid( 3 ),
+    m_x_label("X"),
+    m_y_label("Y"),
+    m_z_label("Z"),
+    m_show_labels( true ),
+    m_show_values( true )
 {
 }
 
@@ -60,6 +69,8 @@ void ParallelCoordinates3DAxis::exec( kvs::ObjectBase* object, kvs::Camera* came
 
     const float dpr = camera->devicePixelRatio();
     this->draw_planes( table, dpr );
+    this->draw_gridlines( table, dpr );
+    this->draw_labels( table, dpr );
 
     BaseClass::stopTimer();
 }
@@ -115,6 +126,149 @@ void ParallelCoordinates3DAxis::draw_planes( const kvs::TableObject* table, cons
         kvs::OpenGL::End();
     }
 */
+}
+    
+void ParallelCoordinates3DAxis::draw_gridlines( const kvs::TableObject* table, const float dpr )
+{
+    const kvs::Vec3 min_coord = table->minObjectCoord();
+    const kvs::Vec3 max_coord = table->maxObjectCoord();
+    float y_interval = ( max_coord.y() - min_coord.y() ) / (m_number_of_grid + 1);
+    float z_interval = ( max_coord.z() - min_coord.z() ) / (m_number_of_grid + 1);
+    for ( size_t i = 0; i < m_plane_positions.size(); i++ )
+    {
+        const float x_coord = m_plane_positions[i].x();
+        kvs::OpenGL::SetLineWidth( m_axis_width * dpr );
+        kvs::OpenGL::Color( m_axis_color );
+        for ( size_t j = 0; j < m_number_of_grid; j++ )
+        {
+            kvs::OpenGL::Begin( GL_LINE_STRIP );
+            kvs::OpenGL::Vertex( x_coord, min_coord.y(), min_coord.z() + z_interval * (j+1) );
+            kvs::OpenGL::Vertex( x_coord, max_coord.y(), min_coord.z() + z_interval * (j+1) );
+            kvs::OpenGL::End();
+            kvs::OpenGL::Begin( GL_LINE_STRIP );
+            kvs::OpenGL::Vertex( x_coord, min_coord.y() + y_interval * (j+1), min_coord.z() );
+            kvs::OpenGL::Vertex( x_coord, min_coord.y() + y_interval * (j+1), max_coord.z() );
+            kvs::OpenGL::End();
+        }
+    }
+}
+    
+void ParallelCoordinates3DAxis::draw_labels( const kvs::TableObject* table, const float dpr )
+{
+    const kvs::Vec3 min_coord = table->minObjectCoord();
+    const kvs::Vec3 max_coord = table->maxObjectCoord();
+    const kvs::Vector3f center = ( max_coord + min_coord ) * 0.5f;
+    
+    // Depth values (Z values) in the camera coordinate system.
+    GLfloat m[16]; glGetFloatv( GL_MODELVIEW_MATRIX, m );
+    const kvs::Real32 x_min = min_coord.x() * m[2] + center.y() * m[6] + center.z() * m[10] + m[14];
+    const kvs::Real32 x_max = max_coord.x() * m[2] + center.y() * m[6] + center.z() * m[10] + m[14];
+    const kvs::Real32 y_min = center.x() * m[2] + min_coord.y() * m[6] + center.z() * m[10] + m[14];
+    const kvs::Real32 y_max = center.x() * m[2] + max_coord.y() * m[6] + center.z() * m[10] + m[14];
+    const kvs::Real32 z_min = center.x() * m[2] + center.y() * m[6] + min_coord.z() * m[10] + m[14];
+    const kvs::Real32 z_max = center.x() * m[2] + center.y() * m[6] + max_coord.z() * m[10] + m[14];
+    
+
+    kvs::Vector3f min_value = min_coord;
+    kvs::Vector3f max_value = max_coord;
+    kvs::Vector3f dvalue = max_value - min_value;
+    dvalue /= static_cast<float>( m_number_of_grid + 1 );
+    
+    kvs::Vector3f interval = max_value - min_value;
+    interval /= static_cast<float>( m_number_of_grid + 1 );
+    
+    const kvs::Vector3f diff( max_coord - min_coord );
+    const kvs::Real32 length = kvs::Math::Max( diff.x(), diff.y(), diff.z() );
+    const kvs::Real32 label_offset = length * 0.1f;
+    const kvs::Real32 value_offset = length * 0.05f;
+    
+    // Draw Y label and values.
+    {
+        kvs::Vector3f position( min_coord.x(), center.y(), min_coord.z() );
+        position.x() = x_min <= x_max ? min_coord.x() : max_coord.x();
+        position.z() = z_min <= z_max ? max_coord.z() : min_coord.z();
+        
+        // Label.
+        if ( m_show_labels )
+        {
+            kvs::Vector3f offset( -label_offset, 0.0f, -label_offset );
+            offset.x() = x_min <= x_max ? -label_offset :  label_offset;
+            offset.z() = z_min <= z_max ?  label_offset : -label_offset;
+            
+            glColor3ub( m_label_color.r(), m_label_color.g(), m_label_color.b() );
+            
+            const kvs::Vector3f label = position + offset;
+            glRasterPos3f( label.x(), label.y(), label.z() );
+            char* head = const_cast<char*>( m_y_label.c_str() );
+            for ( char* p = head; *p; p++ ) { glutBitmapCharacter( GLUT_BITMAP_8_BY_13, *p ); }
+        }
+        
+        // Values.
+        if ( m_show_values )
+        {
+            kvs::Vector3f offset( -value_offset, 0.0f, -value_offset );
+            offset.x() = x_min <= x_max ? -value_offset :  value_offset;
+            offset.z() = z_min <= z_max ?  value_offset : -value_offset;
+            
+            glColor3ub( m_value_color.r(), m_value_color.g(), m_value_color.b() );
+            
+            const size_t nlines = size_t( m_number_of_grid ) + 2;
+            for ( size_t i = 0; i < nlines; i++ )
+            {
+                std::string number = kvs::String::ToString( min_value.y() + dvalue.y() * i );
+                const float x = position.x() + offset.x();
+                const float y = min_coord.y() + interval.y() * i;
+                const float z = position.z() + offset.y();
+                glRasterPos3f( x, y, z );
+                char* head = const_cast<char*>( number.c_str() );
+                for ( char* p = head; *p; p++ ) { glutBitmapCharacter( GLUT_BITMAP_8_BY_13, *p ); }
+            }
+        }
+    }
+    
+    // Draw Z label and values.
+    {
+        kvs::Vector3f position( min_coord.x(), min_coord.y(), center.z() );
+        position.x() = x_min <= x_max ? max_coord.x() : min_coord.x();
+        position.y() = y_min <= y_max ? min_coord.y() : max_coord.y();
+        
+        // Label.
+        if ( m_show_labels )
+        {
+            kvs::Vector3f offset( -label_offset, -label_offset, 0.0f );
+            offset.x() = x_min <= x_max ?  label_offset : -label_offset;
+            offset.y() = y_min <= y_max ? -label_offset :  label_offset;
+            
+            glColor3ub( m_label_color.r(), m_label_color.g(), m_label_color.b() );
+            
+            const kvs::Vector3f label = position + offset;
+            glRasterPos3f( label.x(), label.y(), label.z() );
+            char* head = const_cast<char*>( m_z_label.c_str() );
+            for ( char* p = head; *p; p++ ) { glutBitmapCharacter( GLUT_BITMAP_8_BY_13, *p ); }
+        }
+        
+        // Values.
+        if ( m_show_values )
+        {
+            kvs::Vector3f offset( -value_offset, -value_offset, 0.0f );
+            offset.x() = x_min <= x_max ?  value_offset : -value_offset;
+            offset.y() = y_min <= y_max ? -value_offset :  value_offset;
+            
+            glColor3ub( m_label_color.r(), m_value_color.g(), m_value_color.b() );
+            
+            const size_t nlines = size_t( m_number_of_grid ) + 2;
+            for ( size_t i = 0; i < nlines; i++ )
+            {
+                std::string number = kvs::String::ToString( min_value.z() + dvalue.z() * i );
+                const float x = position.x() + offset.x();
+                const float y = position.y() + offset.y();
+                const float z = min_coord.z() + interval.z() * i;
+                glRasterPos3f( x, y, z );
+                char* head = const_cast<char*>( number.c_str() );
+                for( char* p = head; *p; p++ ) { glutBitmapCharacter( GLUT_BITMAP_8_BY_13, *p ); }
+            }
+        }
+    }
 }
 
 } // end of namespace local
